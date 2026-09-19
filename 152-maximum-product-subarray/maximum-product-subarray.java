b92160ef89e68bb4942030457bbfc291ed16e57b
Anushka Singh <anushkasingh328827@gmail.com>
2026-09-19 23:02:00 +0530

    Time: 2 ms (71.71%) | Memory: 47.4 MB (15.74%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -0,0 +1,30 @@
+ class Solution {
+    public int maxProduct(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+
+        int maxSoFar = nums[0];
+        int minSoFar = nums[0];
+        int result = maxSoFar;
+
+        for (int i = 1; i < nums.length; i++) {
+            int curr = nums[i];
+
+           
+            if (curr < 0) {
+                int temp = maxSoFar;
+                maxSoFar = minSoFar;
+                minSoFar = temp;
+            }
+
+           
+            maxSoFar = Math.max(curr, maxSoFar * curr);
+            minSoFar = Math.min(curr, minSoFar * curr);
+
+           
+            result = Math.max(result, maxSoFar);
+        }
+
+        return result;
+    }
+}  
+    
